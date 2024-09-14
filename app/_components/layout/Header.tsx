b90945d75a49a0cdcdb5116d124b714e5a753829
Kei-dev-1213 <zz2003live@gmail.com>
2024-09-14 17:44:08 +0900

    test

--- app/_components/layout/Header.tsx
+++ app/_components/layout/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
           className={`text-5xl pl-8 ${sacramento.className} inline-block hover:opacity-70`}
           data-testid="header-title"
         >
-          <strong>Next-Tech-Blog2</strong>
+          <strong>Next-Tech-Blog</strong>
         </h1>
       </Link>
     </div>
